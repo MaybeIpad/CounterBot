0f264fbbc315f90761a41033c7e21b525be507f9
MaybeIpad <70214747+MaybeIpad@users.noreply.github.com>
2021-07-10 22:05:12 +0200

    Update CounterBot.py

--- CounterBot.py
+++ CounterBot.py
@@ -35,44 +35,9 @@ async def on_ready():
     print('Logged in as {0.user}, time to start counting!'.format(bot))
     
 @bot.event
-async def on_message(message):
-    global last_number
-    if bot.user.id != message.author.id and message.channel.id == int(counting_channel):
-        channel = bot.get_channel(int(counting_channel))
-        try:
-            if last_number == "" and mode == "INCREMENTAL":
-                last_number = int(message.content)
-            elif last_number == "" and mode == "BINARY":
-                last_number = int(message.content, 2)
-        except:
-            pass
-        else:            
-            if mode == "INCREMENTAL" and int(message.content) < last_number:
-                print("Lower number posted, doing nothing!")
-            elif mode == "INCREMENTAL" and int(message.content) > (last_number + 1):
-                print("too high number, doing nothing!")
-            elif mode == "BINARY" and int(message.content, 2) < last_number:
-                print("Lower number posted, doing nothing!")
-            elif mode == "BINARY" and int(message.content, 2) > (last_number + 1):
-                print("too high number, doing nothing!")
-            else:
-                time.sleep(random.uniform(delay[0], delay[1]))
-                last_number += 2
-                if mode == "INCREMENTAL":
-                    num = int(message.content)
-                    bigger_num = num + 1
-                    
-                    await channel.send(bigger_num)
-                    print("SENDING: " + str(bigger_num))
-                    print("NEXT NUMBER: " + str(last_number))
-                    
-                elif mode == "BINARY":
-                    num = int(message.content, 2)
-                    bigger_num = "{0:b}".format(num + 1)
-                    
-                    await channel.send(bigger_num)
-                    print("SENDING: " + str(bigger_num))
-                    print("NEXT NUMBER: " + str("{0:b}".format(last_number)))
+if message == "based" {
+// ''Your next line is ''based''''
+}
             
 
 try:
